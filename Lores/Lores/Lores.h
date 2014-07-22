18e27e8a6aea41ded65b9664daadf4c515ec4f07
Robert McNally <robert@arciem.com>
2014-07-22 14:42:01 -0700

    First commit.

---
+++ Lores/Lores/Lores.h
@@ -0,0 +1,19 @@
+//
+//  Lores.h
+//  Lores
+//
+//  Created by Robert McNally on 7/22/14.
+//  Copyright (c) 2014 Arciem LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lores.
+FOUNDATION_EXPORT double LoresVersionNumber;
+
+//! Project version string for Lores.
+FOUNDATION_EXPORT const unsigned char LoresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lores/PublicHeader.h>
+
+
